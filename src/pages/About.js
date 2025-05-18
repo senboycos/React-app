df2176472bd9303c8fbaacb7a60ba80f45dbdf13
senboycos <karimsow2013@gmail.com>
2025-05-19 00:37:42 +0200

    navigation test

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>About Page</h1>
+            <p>This is the about page of our application.</p>
+            <p>Here you can find information about our team and the purpose of this app.</p>
+        </div>
+    );
+};
+
+export default About;
