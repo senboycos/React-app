6217e50185de78f89f393542763c35ed8f102dc6
senboycos <karimsow2013@gmail.com>
2025-05-19 01:20:59 +0200

    navigation style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,10 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <ul>
-                <NavLink to="/" >
+                <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : '')}>
                     <li>Home</li>
                 </NavLink>
-                <NavLink to="/about">
+                <NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : '')}>
                     <li>About</li>
                 </NavLink>
             </ul>
